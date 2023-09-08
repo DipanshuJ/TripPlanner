d40c31aed021f7ed62a9639d2d5e6ff02e015cdd
DipanshuJ <103502287+DipanshuJ@users.noreply.github.com>
2023-09-08 11:18:33 +0530

    Design trip class

---
+++ trip.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class trip {
+    private String name;
+    private List<passenger> passengers;
+    private List<place> destinations;
+    private int capacity;
+
+    public trip(String name, int capacity, List<place> destinations){
+        this.name = name;
+        this.passengers = new ArrayList<passenger>();
+        this.destinations = destinations;
+        this.capacity = capacity;
+    }
+
+    public boolean addPassengger(passenger p){
+        if(capacity == passengers.size()) return false;
+        passengers.add(p);
+        return true;
+    }
+
+    public void passengerInformation(){
+        System.out.println("Trip Package name: " + name);
+        System.out.println("Trip Package capacity: " + capacity);
+        System.out.println("Destinations list: ");
+        for(int i=0;i<destinations.size();i++){
+            System.out.println((i+1) + ". " + destinations.get(i));
+        }
+        System.out.println("Passengers Information: ");
+        for(int i=0;i<passengers.size();i++){
+            System.out.print((i+1) + ". ");
+            passengers.get(i).getInformation();
+        }
+    }
+
+    public void packageDetails(){
+        System.out.println("Trip Package name: " + name);
+        System.out.println("Trip Package capacity: " + capacity);
+        System.out.println("Destinations: ");
+        for(int i=0;i<destinations.size();i++){
+            System.out.println((i+1) + ". " + destinations.get(i));
+        }
+    }
+}
